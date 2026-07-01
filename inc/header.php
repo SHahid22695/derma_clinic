c9f4d531d99688fe47c27963a1dd8dc2a3d4da6a
SHahid22695 <shhdmhmdsafd@gmail.com>
2026-07-01 19:01:25 +0300

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -5,14 +5,14 @@ $nav_class = ($page == 'index.php') ? 'ftco-navbar-light' : 'ftco-navbar-light-2
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-
-
   <head>
     <title>Sparlex - Beauty Clinic</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Prata&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
@@ -27,15 +27,20 @@ $nav_class = ($page == 'index.php') ? 'ftco-navbar-light' : 'ftco-navbar-light-2
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <nav <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark" id="ftco-navbar">
+    
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark <?php echo $nav_class; ?>" id="ftco-navbar">
 
       <div class="container">
+        
         <a class="navbar-brand" href="index.php"><span class="flaticon-lotus"></span>Sparlex</a>
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="oi oi-menu"></span> Menu
         </button>
+        
         <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
+            <!--  تفعيل الرابط النشط -->
             <li class="nav-item <?php if($page == 'index.php') echo 'active'; ?>"><a href="index.php" class="nav-link">Home</a></li>
             <li class="nav-item <?php if($page == 'about.php') echo 'active'; ?>"><a href="about.php" class="nav-link">About</a></li>
             <li class="nav-item <?php if($page == 'service.php') echo 'active'; ?>"><a href="service.php" class="nav-link">Treatments</a></li>
@@ -43,6 +48,7 @@ $nav_class = ($page == 'index.php') ? 'ftco-navbar-light' : 'ftco-navbar-light-2
             <li class="nav-item <?php if($page == 'price.php') echo 'active'; ?>"><a href="price.php" class="nav-link">Pricing</a></li>
             <li class="nav-item <?php if($page == 'booking.php') echo 'active'; ?>"><a href="booking.php" class="nav-link">Appointment</a></li>
             <li class="nav-item <?php if($page == 'contact.php') echo 'active'; ?>"><a href="contact.php" class="nav-link">Contact</a></li>
+            
             <?php if(isset($_SESSION['logged']) && $_SESSION['logged'] === TRUE): ?>
             <li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
             <?php else: ?>
